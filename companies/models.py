a899db5fbfd1573514c4875a3468b90c0da0c61a
mhrnciar <matej.hrnciar2@gmail.com>
2021-03-18 13:55:56 +0100

    Add create and insert into companies

--- companies/models.py
+++ companies/models.py
@@ -1,15 +1,3 @@
 from django.db import models
 
-
 # Create your models here.
-class Companies(models.Model):
-    cin = models.BigIntegerField(primary_key=True)
-    name = models.CharField(max_length=300, null=True)
-    br_section = models.CharField(max_length=50, null=True)
-    address_line = models.CharField(max_length=300, null=True)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-    last_update = models.DateTimeField()
-
-    def __str__(self):
-        return self.name
--- companies/views.py
+++ companies/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
 # Create your views here.
-def index():
+def index(request):
     pass
